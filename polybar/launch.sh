f70dd2e8af288ba269918396e8c0ea7736cc7863
keshav13142 <s.keshav13142@gmail.com>
2023-04-27 18:14:33 +0530

    Updated i3 and polybar config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,4 +8,4 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar --config=~/.config/polybar/config.ini bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar --config=~/.config/polybar/config.ini personal 2>&1 | tee -a /tmp/polybar1.log & disown
