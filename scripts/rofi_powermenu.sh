c9752781f2e4da28c213361542c769b98058a0bd
keshav13142 <s.keshav13142@gmail.com>
2023-04-28 08:43:49 +0530

    Added scripts for rofi

---
+++ scripts/rofi_powermenu.sh
@@ -0,0 +1,8 @@
+case "$(printf " Power off\n Restart\n Lock\n Sleep\n Logout" | rofi -dmenu -i -theme-str '@import "~/.config/rofi/power.rasi"')" in
+  " Power off") systemctl poweroff ;;
+  " Restart") systemctl reboot ;;
+  " sleep") systemctl suspend ;;
+  " Lock") betterlockscreen -l ;;
+  " Logout") i3-msg exit ;;
+  *) exit 1 ;;
+esac
