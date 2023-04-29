2a03db79ee5ee520d3d2337cf268bd51d005d05e
keshav13142 <s.keshav13142@gmail.com>
2023-04-29 12:17:42 +0530

    show active profiles

--- scripts/power_profiles_menu.sh
+++ scripts/power_profiles_menu.sh
@@ -1,8 +1,8 @@
-chosen=$(printf " Battery\n Balanced\n Performance\n" | rofi -dmenu -i -theme-str '@import "power.rasi"')
+chosen=$(printf '  Battery\n  Balanced\n  Performance\n%s (active)' "$(system76-power profile | grep -oP 'Power Profile: \K\w+')" | rofi -dmenu -i -theme-str '@import "power.rasi"')
 
 case "$chosen" in
-	" Battery") system76-power profile battery ;;
-	" Balanced") system76-power profile balanced ;;
-	" Performance") system76-power profile performance ;;
+	"  Battery") system76-power profile battery ;;
+	"  Balanced") system76-power profile balanced ;;
+	"  Performance") system76-power profile performance ;;
 	*) exit 1 ;;
 esac
