fdec3856b516e57cf6304234593454a95ddba32f
keshav13142 <s.keshav13142@gmail.com>
2023-05-19 22:19:35 +0530

    updated lvim config and added pulgins

--- lvim/config.lua
+++ lvim/config.lua
@@ -5,10 +5,6 @@ vim.opt.relativenumber = true
 vim.opt.incsearch = true
 vim.opt.wrap = true -- wrap lines
 
--- use treesitter folding
--- vim.opt.foldmethod = "expr"
--- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -16,6 +12,7 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Enable Transparent Windows
 lvim.transparent_window = true
+lvim.builtin.terminal.open_mapping = "<C-t>"
 
 -- General
 lvim.log.level = "info"
@@ -28,6 +25,12 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- Cycle between tabs using H and L
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+lvim.keys.normal_mode["<C-s>"] = ":w<CR>"
+lvim.keys.insert_mode["<C-s>"] = "<ESC>:w<CR>"
+lvim.keys.normal_mode["<C-a>"] = "ggVG"
+lvim.keys.insert_mode["<C-a>"] = "<ESC>ggVG"
+lvim.keys.normal_mode["<C-q>"] = ":q!<CR>"
+lvim.keys.insert_mode["<C-q>"] = "<ESC>:q!<CR>"
 
 -- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.which_key.mappings["W"] = { "<cmd>noautocmd w<cr>", "Save without formatting" }
@@ -56,24 +59,92 @@ vim.filetype.add {
   },
 }
 
---  Formatting config
--- local nls = require("null-ls")
--- nls.setup {
---   on_attach = require("lvim.lsp").common_on_attach,
---   debounce = 150,
---   save_after_format = false,
---   sources = {
---     nls.builtins.formatting.prettierd.with {
---       condition = function(utils)
---         return not utils.root_has_file { ".eslintrc", ".eslintrc.js" }
---       end,
---       prefer_local = "node_modules/.bin",
---     },
---     nls.builtins.formatting.eslint_d.with {
---       condition = function(utils)
---         return utils.root_has_file { ".eslintrc", ".eslintrc.js" }
---       end,
---       prefer_local = "node_modules/.bin",
---     },
---   },
--- }
+lvim.plugins = {
+  {
+    "ggandor/leap.nvim",
+    name = "leap",
+    config = function()
+      require("leap").add_default_mappings()
+    end,
+  },
+  -- {
+  --   "andymass/vim-matchup",
+  --   event = "CursorMoved",
+  --   config = function()
+  --     vim.g.matchup_matchparen_offscreen = { method = "popup" }
+  --   end,
+  -- },
+  {
+    "folke/trouble.nvim",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    cmd = "TroubleToggle",
+    config = function()
+      require("trouble").setup({
+        height = 5,
+        padding = false,
+      })
+    end,
+  },
+  { "ellisonleao/glow.nvim",   ft = { "markdown" } },
+  { "ray-x/lsp_signature.nvim" },
+  { "folke/zen-mode.nvim" },
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup({ "*" }, {
+        RGB = true,      -- #RGB hex codes
+        RRGGBB = true,   -- #RRGGBB hex codes
+        RRGGBBAA = true, -- #RRGGBBAA hex codes
+        rgb_fn = true,   -- CSS rgb() and rgba() functions
+        hsl_fn = true,   -- CSS hsl() and hsla() functions
+        css = true,      -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        css_fn = true,   -- Enable all CSS *functions*: rgb_fn, hsl_fn
+      })
+    end,
+  },
+  { "sindrets/diffview.nvim", dependencies = "nvim-lua/plenary.nvim" },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = "nvim-lua/plenary.nvim",
+    config = function()
+      require("todo-comments").setup({})
+    end,
+  },
+  {
+    'lukas-reineke/headlines.nvim',
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = true
+  },
+  { "kylechui/nvim-surround" },
+  {
+    "tpope/vim-fugitive",
+    cmd = {
+      "G",
+      "Git",
+      "Gdiffsplit",
+      "Gread",
+      "Gwrite",
+      "Ggrep",
+      "GMove",
+      "GDelete",
+      "GBrowse",
+      "GRemove",
+      "GRename",
+      "Glgrep",
+      "Gedit"
+    },
+    ft = { "fugitive" }
+  },
+  {
+    "felipec/vim-sanegx",
+    event = "BufRead",
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+}
+
+lvim.builtin.treesitter.autotag.enable = true
