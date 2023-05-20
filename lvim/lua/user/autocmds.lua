53c93eaed12805caefa6410091e013ac96233503
keshav13142 <s.keshav13142@gmail.com>
2023-05-20 23:28:05 +0530

    Added autocmds for certain file types

---
+++ lvim/lua/user/autocmds.lua
@@ -0,0 +1,23 @@
+vim.api.nvim_create_autocmd("BufWritePost", {
+	pattern = { "*tmux.conf" },
+	command = "execute 'silent !tmux source <afile> --silent'",
+})
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+	pattern = { ".skhdrc" },
+	command = "!brew services restart skhd",
+})
+
+-- vim.api.nvim_create_autocmd({ "BufNewFile", "BufFilePre", "BufRead" }, {
+--   pattern = { "*.mdx", "*.md" },
+--   callback = function()
+--     vim.cmd([[set filetype=markdown wrap linebreak nolist]])
+--   end,
+-- })
+
+vim.api.nvim_create_autocmd({ "BufRead" }, {
+	pattern = { "*.conf" },
+	callback = function()
+		vim.cmd([[set filetype=sh]])
+	end,
+})
