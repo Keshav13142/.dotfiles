cf7da55a18abd7434eacd2a49ea5ca8644d8a80d
Keshav S <s.keshav13142@gmail.com>
2023-06-12 14:26:43 +0530

    nvim: neo-tree config

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,6 +1,32 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  opts = function(_, opts)
-    opts.window.position = "right"
-  end,
+  opts = {
+    sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+    open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "Outline" },
+    filesystem = {
+      bind_to_cwd = false,
+      follow_current_file = true,
+      use_libuv_file_watcher = true,
+      hide_gitignored = false,
+      visible = true,
+      hide_dotfiles = false,
+      hide_by_name = {
+        "node_modules",
+      },
+    },
+    window = {
+      position = "right",
+      mappings = {
+        ["<space>"] = "none",
+      },
+    },
+    default_component_configs = {
+      indent = {
+        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+        expander_collapsed = "",
+        expander_expanded = "",
+        expander_highlight = "NeoTreeExpander",
+      },
+    },
+  },
 }
