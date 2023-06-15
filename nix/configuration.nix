f52ea64ee54d18e2a1bc10bed5652e5173533c28
Keshav S <s.keshav13142@gmail.com>
2023-06-15 08:50:32 +0530

    nix: format and fix stuff

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -3,24 +3,24 @@
 {
   imports =
     [
-      # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
 
   # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/vda";
-  boot.loader.grub.useOSProber = true;
+  boot.loader = {
+    grub = {
+      enable = true;
+      device = "/dev/vda";
+      useOSProber = true;
+    };
+  };
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
+  networking = {
+    firewall.enable = false;
+    enableIPv6 = false;
+    hostName = "nixos";
+    networkmanager.enable = true;
+  };
 
   # Set your time zone.
   time.timeZone = "Asia/Kolkata";
@@ -40,9 +40,70 @@
     LC_TIME = "en_IN";
   };
 
-  nixpkgs.config.allowUnfree = true;
+  users.users.keshav = {
+    isNormalUser = true;
+    description = "keshav";
+    extraGroups = [ "networkmanager" "wheel" "kvm" "input" "disk" "libvirtd" ];
+  };
 
-  # Enable the X11 windowing system.
+  # List packages installed in system profile.
+  environment.systemPackages = with pkgs; [
+    alacritty
+    bat
+    betterlockscreen
+    brave
+    brillo
+    cargo
+    clang-tools_9
+    clipmenu
+    dunst
+    exa
+    feh
+    flatpak
+    gcc
+    # i3
+    gimp
+    git
+    gnumake
+    gparted
+    lazygit
+    neofetch
+    neovim
+    nfs-utils
+    ninja
+    nixpkgs-fmt
+    nodejs
+    openssl
+    pavucontrol
+    picom
+    polkit_gnome
+    polybar
+    python310Packages.pip
+    python3Full
+    qemu
+    ripgrep
+    rofi
+    scrot
+    stdenv
+    sxhkd
+    tldr
+    tmux
+    trash-cli
+    unzip
+    virt-manager
+    vscode
+    wget
+    xclip
+    xdg-desktop-portal-gtk
+    xorg.libX11
+    xorg.libX11.dev
+    xorg.libxcb
+    xorg.libXft
+    xorg.libXinerama
+    xorg.xinit
+    xorg.xinput
+    zoxide
+  ];
 
   # i3 configuration
   services.xserver = {
@@ -53,124 +114,47 @@
     displayManager.defaultSession = "none+i3";
     windowManager.i3 = {
       enable = true;
-      package = pkgs.i3;
+      # package = pkgs.i3;
     };
     layout = "us";
     xkbVariant = "intl";
     libinput.enable = true;
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+  services = {
+    printing.enable = true;
+    flatpak.enable = true;
+    dbus.enable = true;
+    picom.enable = true;
+    # clipmenud.enable = true;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
+  };
 
-  services.picom.enable = true;
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
 
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
+  security = {
+    rtkit.enable = true;
+    polkit.enable = true;
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  virtualisation.libvirtd.enable = true;
+
   users.defaultUserShell = pkgs.zsh;
-
-  users.users.keshav = {
-    isNormalUser = true;
-    description = "keshav";
-    extraGroups = [ "networkmanager" "wheel" "kvm" "input" "disk" "libvirtd" ];
-  };
-
-  # List packages installed in system profile. To search, run:
-  environment.systemPackages = with pkgs; [
-    brave
-    i3
-    polybar
-    bat
-    zoxide
-    alacritty
-    lazygit
-    zsh
-    tmux
-    git
-    neovim
-    wget
-    neofetch
-    neovim
-    autojump
-    cargo
-    clang-tools_9
-    dunst
-    feh
-    vscode
-    exa
-    nodejs_20
-    scrot
-    sxhkd
-    clipmenu
-    brillo
-    flatpak
-    fontconfig
-    freetype
-    gcc
-    gh
-    gimp
-    gnumake
-    gparted
-    nfs-utils
-    ninja
-    openssl
-    pavucontrol
-    picom
-    nixpkgs-fmt
-    polkit_gnome
-    python3Full
-    pkgs.python310Packages.pip
-    qemu
-    ripgrep
-    rofi
-    sxhkd
-    st
-    stdenv
-    terminus-nerdfont
-    tldr
-    trash-cli
-    unzip
-    variety
-    virt-manager
-    xclip
-    xdg-desktop-portal-gtk
-    xorg.libX11
-    xorg.libX11.dev
-    xorg.libxcb
-    xorg.libXft
-    xorg.libXinerama
-    xorg.xinit
-    xorg.xinput
-  ];
-
   programs.zsh.enable = true;
+
 
   xdg.portal = {
     enable = true;
-    # wlr.enable = true;
-    # gtk portal needed to make gtk apps happy
     extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
-
-  # List services that you want to enable:
-  virtualisation.libvirtd.enable = true;
-  # enable flatpak support
-  services.flatpak.enable = true;
-  services.dbus.enable = true;
-  #services.clipmenud.enable = true;
-
-  security.polkit.enable = true;
 
   systemd = {
     user.services.polkit-gnome-authentication-agent-1 = {
@@ -187,7 +171,7 @@
       };
     };
     extraConfig = ''
-      DefaultTimeoutStopSec=10s
+      DefaultTimeoutStopSec=1s
     '';
   };
 
@@ -197,19 +181,14 @@
         i3Support = true;
       };
     };
+    allowUnfree = true;
   };
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = false;
-  networking.enableIPv6 = false;
-
-  system.copySystemConfiguration = true;
-  system.autoUpgrade.enable = true;
-  system.autoUpgrade.allowReboot = true;
-  system.autoUpgrade.channel = "https://channels.nixos.org/nixos-23.05";
-
-  system.stateVersion = "23.05"; # Did you read the comment?
+  system = {
+    copySystemConfiguration = true;
+    autoUpgrade.enable = true;
+    autoUpgrade.allowReboot = true;
+    autoUpgrade.channel = "https://channels.nixos.org/nixos-23.05";
+    stateVersion = "23.05";
+  };
 }
