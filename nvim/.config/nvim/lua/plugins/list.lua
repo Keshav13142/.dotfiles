2cd7c629455c4739511e335cb3de774459ff252e
Keshav S <s.keshav13142@gmail.com>
2023-06-19 21:08:25 +0530

    nvim: remove redundant plugin

--- nvim/.config/nvim/lua/plugins/list.lua
+++ nvim/.config/nvim/lua/plugins/list.lua
@@ -7,19 +7,6 @@ return {
   "tpope/vim-unimpaired",
   "tpope/vim-repeat",
   {
-    "jose-elias-alvarez/typescript.nvim",
-    config = function()
-      require("typescript").setup({
-        disable_commands = false, -- prevent the plugin from creating Vim commands
-        debug = false, -- enable debug logging for commands
-        go_to_source_definition = {
-          fallback = true, -- fall back to standard LSP definition on failure
-        },
-        server = {},
-      })
-    end,
-  },
-  {
     "felipec/vim-sanegx",
     event = "BufRead",
   },
