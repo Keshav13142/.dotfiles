69937038f629367313037cbe5da27dc2a9f6e398
Keshav S <s.keshav13142@gmail.com>
2023-07-05 06:33:13 +0530

    zsh: increase fzf height

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -54,9 +54,9 @@ _gitLogLineToHash="echo {} | grep -o '[a-f0-9]\{7\}' | head -1"
 _viewGitLogLine="$_gitLogLineToHash | xargs -I % sh -c 'git show --color=always % | diff-so-fancy'"
 
 # git commit browser with previews
-fshow() {
+fgc() {
   git log --color=always --format="%C(auto)%h%d %s %C(black)%C(bold)%cr% C(auto)%an" "$@" |
-    fzf --no-sort --reverse --tiebreak=index --no-multi \
+    fzf --no-sort --reverse --height=100% --tiebreak=index --no-multi \
       --ansi --preview="$_viewGitLogLine" \
       --header "enter to view, alt-y to copy hash" \
       --bind "enter:execute:$_viewGitLogLine   | less -R" \
