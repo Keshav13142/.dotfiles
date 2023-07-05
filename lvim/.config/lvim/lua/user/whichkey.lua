4b8e126a4a250ca92498f794587d7d60d01f62af
Keshav S <s.keshav13142@gmail.com>
2023-07-05 17:35:37 +0530

    lvim: remove unused which-key mappings

--- lvim/.config/lvim/lua/user/whichkey.lua
+++ lvim/.config/lvim/lua/user/whichkey.lua
@@ -1,22 +1,11 @@
-lvim.builtin.which_key.mappings["f"] = {
-	name = "Find",
-	b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
-	c = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
-	f = { "<cmd>Telescope find_files<cr>", "Find files" },
-	g = { "<cmd>Telescope git_files<cr>", "Find git files" },
-	t = { "<cmd>Telescope live_grep<cr>", "Find Text" },
-	s = { "<cmd>Telescope grep_string<cr>", "Find String" },
-	h = { "<cmd>Telescope help_tags<cr>", "Help" },
-	H = { "<cmd>Telescope highlights<cr>", "Highlights" },
-	i = { "<cmd>lua require('telescope').extensions.media_files.media_files()<cr>", "Media" },
-	l = { "<cmd>Telescope resume<cr>", "Last Search" },
-	M = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
-	r = { "<cmd>Telescope oldfiles<cr>", "Recent File" },
-	R = { "<cmd>Telescope registers<cr>", "Registers" },
-	k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
-	C = { "<cmd>Telescope commands<cr>", "Commands" },
-}
+-- Disable stuff I use less often
+lvim.builtin.which_key.mappings["d"] = nil
+lvim.builtin.which_key.mappings["p"] = nil
+lvim.builtin.which_key.mappings[";"] = nil
+lvim.builtin.which_key.mappings["T"] = nil
+lvim.builtin.which_key.mappings["/"] = nil
 
+-- TODO: Find out why spectre is not working
 lvim.builtin.which_key.mappings["r"] = {
 	name = "Replace",
 	r = { "<cmd>lua require('spectre').open()<cr>", "Replace" },
@@ -24,7 +13,91 @@ lvim.builtin.which_key.mappings["r"] = {
 	f = { "<cmd>lua require('spectre').open_file_search()<cr>", "Replace Buffer" },
 }
 
-lvim.builtin.which_key.mappings["s"] = nil
+lvim.builtin.which_key.mappings["b"] = {
+	name = "Buffer",
+	b = { "<cmd>BufferLinePick<cr>", "Jump" },
+	f = { "<cmd>Telescope buffers previewer=false<cr>", "Find" },
+	e = {
+		"<cmd>BufferLinePickClose<cr>",
+		"Pick which buffer to close",
+	},
+	h = { "<cmd>BufferLineCloseLeft<cr>", "Close all to the left" },
+	l = {
+		"<cmd>BufferLineCloseRight<cr>",
+		"Close all to the right",
+	},
+}
+
+lvim.builtin.which_key.mappings["L"] = {
+	name = "+LunarVim",
+	c = {
+		"<cmd>edit " .. get_config_dir() .. "/config.lua<cr>",
+		"Edit config.lua",
+	},
+	u = { "<cmd>LvimUpdate<cr>", "Update LunarVim" },
+}
+
+lvim.builtin.which_key.mappings["s"] = {
+	name = "Search",
+	c = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
+	f = { "<cmd>Telescope find_files<cr>", "Find File" },
+	h = { "<cmd>Telescope help_tags<cr>", "Find Help" },
+	M = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
+	r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
+	R = { "<cmd>Telescope registers<cr>", "Registers" },
+	t = { "<cmd>Telescope live_grep<cr>", "Text" },
+	k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
+	l = { "<cmd>Telescope resume<cr>", "Resume last search" },
+}
+
+lvim.builtin.which_key.mappings["l"] = {
+	name = "LSP",
+	a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
+	d = { "<cmd>Telescope diagnostics bufnr=0 theme=get_ivy<cr>", "Buffer Diagnostics" },
+	w = { "<cmd>Telescope diagnostics<cr>", "Diagnostics" },
+	f = { "<cmd>lua require('lvim.lsp.utils').format()<cr>", "Format" },
+	i = { "<cmd>LspInfo<cr>", "Info" },
+	j = {
+		"<cmd>lua vim.diagnostic.goto_next()<cr>",
+		"Next Diagnostic",
+	},
+	k = {
+		"<cmd>lua vim.diagnostic.goto_prev()<cr>",
+		"Prev Diagnostic",
+	},
+	l = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
+	q = { "<cmd>lua vim.diagnostic.setloclist()<cr>", "Quickfix" },
+	r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
+	e = { "<cmd>Telescope quickfix<cr>", "Telescope Quickfix" },
+}
+
+lvim.builtin.which_key.mappings["g"] = {
+	name = "Git",
+	g = { "<cmd>lua require 'lvim.core.terminal'.lazygit_toggle()<cr>", "Lazygit" },
+	j = { "<cmd>lua require 'gitsigns'.next_hunk({navigation_message = false})<cr>", "Next Hunk" },
+	k = { "<cmd>lua require 'gitsigns'.prev_hunk({navigation_message = false})<cr>", "Prev Hunk" },
+	l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
+	p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
+	r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
+	R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
+	s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
+	u = {
+		"<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
+		"Undo Stage Hunk",
+	},
+	o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
+	b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+	c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
+	C = {
+		"<cmd>Telescope git_bcommits<cr>",
+		"Checkout commit(for current file)",
+	},
+	d = {
+		"<cmd>Gitsigns diffthis HEAD<cr>",
+		"Git Diff",
+	},
+}
+
 -- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.which_key.mappings["W"] = { "<cmd>noautocmd w<cr>", "Save without formatting" }
 lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
