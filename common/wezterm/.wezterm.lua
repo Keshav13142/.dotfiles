ab487c6203f030a31191bc792aae50dae3670a61
Keshav <s.keshav13142@gmail.com>
2024-03-03 08:30:26 +0530

    wezterm: show tab index

--- common/wezterm/.wezterm.lua
+++ common/wezterm/.wezterm.lua
@@ -89,7 +89,7 @@ return {
 	-- Tab bar
 	hide_tab_bar_if_only_one_tab = true,
 	show_new_tab_button_in_tab_bar = false,
-	show_tab_index_in_tab_bar = false,
+	show_tab_index_in_tab_bar = true,
 	show_tabs_in_tab_bar = true,
 	tab_bar_at_bottom = true,
 	use_fancy_tab_bar = false,
