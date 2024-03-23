73b64df8bc0bb46c18481cd9222845d96840a42c
Keshav <s.keshav13142@gmail.com>
2024-03-23 11:41:38 +0530

    nix: change formatting

--- linux/nix/nixos/laptop/home.nix
+++ linux/nix/nixos/laptop/home.nix
@@ -4,135 +4,134 @@ let
 in
 {
   imports = [ spicetify-nix.homeManagerModule ];
-  home =
-    {
-      username = "${user}";
-      homeDirectory = "/home/${user}";
-      packages = with pkgs; [
-        # Applications
-        amberol
-        authy
-        bitwarden
-        brave
-        espanso
-        floorp
-        gimp
-        gnome.seahorse # gui secrets manager
-        libreoffice-qt
-        mpv
-        obsidian
-        picard
-        virt-manager
+  home = {
+    username = "${user}";
+    homeDirectory = "/home/${user}";
+    packages = with pkgs; [
+      # Applications
+      amberol
+      authy
+      bitwarden
+      brave
+      espanso
+      floorp
+      gimp
+      gnome.seahorse # gui secrets manager
+      libreoffice-qt
+      mpv
+      obsidian
+      picard
+      virt-manager
 
-        # Utils
-        acpi
-        alsa-lib
-        cava
-        ffmpeg
-        gparted
-        graphicsmagick
-        imagemagick
-        libnotify
-        libpcap
-        nfs-utils
-        pciutils
-        polkit_gnome
-        powertop
-        pulseaudioFull
-        qemu
-        quickemu
-        usbutils
-        kanata
+      # Utils
+      acpi
+      alsa-lib
+      cava
+      ffmpeg
+      gparted
+      graphicsmagick
+      imagemagick
+      libnotify
+      libpcap
+      nfs-utils
+      pciutils
+      polkit_gnome
+      powertop
+      pulseaudioFull
+      qemu
+      quickemu
+      usbutils
+      kanata
 
-        # Dev
-        android-studio
-        android-tools
-        dbeaver
-        mysql-workbench
-        docker
-        docker-compose
-        vscode
+      # Dev
+      android-studio
+      android-tools
+      dbeaver
+      mysql-workbench
+      docker
+      docker-compose
+      vscode
 
-        # Desktop
-        brillo
-        dmenu
-        dunst
-        gnome.cheese
-        gnome.nautilus
-        gtk3
-        kitty
-        networkmanagerapplet
-        pamixer
-        pavucontrol
-        playerctl
-        pywal
-        rofimoji
-        wezterm
-        xdg-desktop-portal
-        xdg-desktop-portal-gtk
-        xfce.thunar
+      # Desktop
+      brillo
+      dmenu
+      dunst
+      gnome.cheese
+      gnome.nautilus
+      gtk3
+      kitty
+      networkmanagerapplet
+      pamixer
+      pavucontrol
+      playerctl
+      pywal
+      rofimoji
+      wezterm
+      xdg-desktop-portal
+      xdg-desktop-portal-gtk
+      xfce.thunar
 
-        # Wayland
-        cliphist
-        glib
-        grim
-        hyprpicker
-        libsForQt5.qt5.qtwayland
-        (rofi-wayland.override {
-          plugins = [
-            pkgs.rofi-calc
-          ];
-        })
-        slurp
-        swappy
-        swayidle
-        swaylock-effects
-        swww
-        wev
-        (waybar.overrideAttrs (oldAttrs: {
-          mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
-        }))
-        wayland-protocols
-        wl-clipboard
-        wlogout
-        wlr-randr
+      # Wayland
+      cliphist
+      glib
+      grim
+      hyprpicker
+      libsForQt5.qt5.qtwayland
+      (rofi-wayland.override {
+        plugins = [
+          pkgs.rofi-calc
+        ];
+      })
+      slurp
+      swappy
+      swayidle
+      swaylock-effects
+      swww
+      wev
+      (waybar.overrideAttrs (oldAttrs: {
+        mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+      }))
+      wayland-protocols
+      wl-clipboard
+      wlogout
+      wlr-randr
 
-        # Xorg/X11
-        # xclip
-        # xorg.xev
-        # lxappearance
-        # picom
-        # (polybar.override {
-        #   i3Support = true;
-        # })
-        # (rofi.override {
-        #   plugins = [
-        #     pkgs.rofi-calc
-        #   ];
-        # })
-        # sxhkd
-        # xdotool
-        # xorg.libX11
-        # xorg.libX11.dev
-        # xorg.libxcb
-        # xorg.libXft
-        # xorg.libXinerama
-        # xorg.xbacklight
-        # xorg.xinit
-        # xorg.xinput
-        # xorg.xkill
-        # xorg.xmodmap
-        # xsel
-      ];
+      # Xorg/X11
+      # xclip
+      # xorg.xev
+      # lxappearance
+      # picom
+      # (polybar.override {
+      #   i3Support = true;
+      # })
+      # (rofi.override {
+      #   plugins = [
+      #     pkgs.rofi-calc
+      #   ];
+      # })
+      # sxhkd
+      # xdotool
+      # xorg.libX11
+      # xorg.libX11.dev
+      # xorg.libxcb
+      # xorg.libXft
+      # xorg.libXinerama
+      # xorg.xbacklight
+      # xorg.xinit
+      # xorg.xinput
+      # xorg.xkill
+      # xorg.xmodmap
+      # xsel
+    ];
 
-      stateVersion = "23.05";
-      pointerCursor = {
-        gtk.enable = true;
-        package = pkgs.gnome.adwaita-icon-theme;
-        name = "Adwaita";
-        size = 16;
-      };
+    stateVersion = "23.05";
+    pointerCursor = {
+      gtk.enable = true;
+      package = pkgs.gnome.adwaita-icon-theme;
+      name = "Adwaita";
+      size = 16;
     };
+  };
 
   services = {
     syncthing.enable = true;
