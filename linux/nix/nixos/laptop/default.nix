7b2958a6f543cb3a6233cdedfb79738f0f8bed85
Keshav <s.keshav13142@gmail.com>
2024-06-30 18:14:17 +0530

    nix:update flake

--- linux/nix/nixos/laptop/default.nix
+++ linux/nix/nixos/laptop/default.nix
@@ -202,8 +202,8 @@
     bluetooth.enable = true;
     opengl = {
       enable = true;
-      driSupport = true;
-      driSupport32Bit = true;
+      # driSupport = true;
+      # driSupport32Bit = true;
       # extraPackages = with pkgs; [
       #   nvidia-vaapi-driver
       #   vaapiVdpau
