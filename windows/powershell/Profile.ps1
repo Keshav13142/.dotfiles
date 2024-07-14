420282f5a6f9f45294983d488d38883eb7b9c842
Keshav <s.keshav13142@gmail.com>
2024-07-14 09:17:26 +0530

    pwsh command not found module

--- windows/powershell/Profile.ps1
+++ windows/powershell/Profile.ps1
@@ -25,6 +25,9 @@ Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory
 Set-PsFzfOption -EnableFd:$true
 Set-PsFzfOption -TabExpansion
 
+fnm env --use-on-cd | Out-String | Invoke-Expression
+
+
 Invoke-Expression (& {
     $hook = if ($PSVersionTable.PSVersion.Major -lt 6) {
       'prompt'
@@ -308,3 +311,18 @@ Set-PSReadLineKeyHandler -Key Ctrl+V `
 
 ######################## Exports #########################
 Set-Environment "KOMOREBI_CONFIG_HOME" "$ENV:USERPROFILE\.config\komorebi"
+
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+#f45873b3-b655-43a6-b217-97c00aa0db58
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
