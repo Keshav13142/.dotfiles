564918495e6c618eff8c94070ff281f6c0df976f
Keshav <s.keshav13142@gmail.com>
2025-03-31 11:06:54 +0530

    updates to kanata autostart_script

--- common/kanata/kanata_autostart_setup.ps1
+++ common/kanata/kanata_autostart_setup.ps1
@@ -3,3 +3,5 @@ $KanataDirectoryPath = "$env:UserProfile\.config\kanata"
 $KanataExePath = $(Get-Command -Name "kanata" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue)
 
 New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "Kanata" -Value "`"C:\Windows\System32\conhost.exe`" --headless `"$KanataExePath`" --cfg `"$KanataDirectoryPath\kanata.kbd`""
+
+Add-MpPreference -ExclusionPath "$KanataExePath"
