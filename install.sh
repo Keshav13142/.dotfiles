e7299bc835425e6deddff83a8cd01e30f627b39e
Keshav <s.keshav13142@gmail.com>
2025-08-04 08:57:47 +0530

    fix libsecret issues I guess
    need to edit /etc/pam.d/login and /etc/pam.d/ssdm for this to work.
    
    auth optional pam_gnome_keyring.so
    session optional pam_gnome_keyring.so auto_start

--- install.sh
+++ install.sh
@@ -15,7 +15,9 @@ cd ~
 # paru -Syu
 # For steam choose the lib nvida provder
 
-paru -S --noconfirm --needed --skipreview --cleanafter amberol bitwarden gimp libreoffice-fresh seahorse mpv zed graphicsmagick yt-dlp imagemagick powertop kanata-bin dmenu cheese nautilus playerctl rofimoji btop obs-studio obsidian zathura cowsay figlet ghostscript glow gum lf lolcat ninja pistol-bin slides-bin libspeechd tig mediainfo sesh fd jq tldr trash-cli stow tree unrar zip statix-git gofumpt glib pnpm unzip brave-bin kitty firefox zoxide git neovim tmux go python lazygit brillo sxhkd kdeconnect cargo-binstall syncthing flameshot intellij-idea-community-edition cava visual-studio-code-bin redshift python-pywal xorg-xev acpid blueman cups cups-pdf cups-filters system-config-printer gnome-themes-extra kvantum-qt5 clang mise mariadb-libs postgresql-libs lazydocker-bin wget curl inetutils eza fzf ripgrep bat fastfetch man less whois plocate bash-completion gnome-calculator localsend-bin bun-bin discord ufw powershell-bin starship docker sqlc docker-compose mongodb-bin mongodb-tools-bin mongodb-compass-bin flatpak optimus-manager nvtop libva-utils polybar picom ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols networkmanager network-manager-applet nm-connection-editor wireless_tools wpa_supplicant
+paru -S --noconfirm --needed --skipreview --cleanafter amberol bitwarden gimp libreoffice-fresh seahorse mpv zed graphicsmagick yt-dlp imagemagick powertop kanata-bin dmenu cheese nautilus playerctl rofimoji btop obs-studio obsidian zathura cowsay figlet ghostscript glow gum lf lolcat ninja pistol-bin slides-bin libspeechd tig mediainfo sesh fd jq tldr trash-cli stow tree unrar zip statix-git gofumpt glib pnpm unzip brave-bin kitty firefox zoxide git neovim tmux go python lazygit brillo kdeconnect cargo-binstall syncthing flameshot intellij-idea-community-edition cava visual-studio-code-bin redshift python-pywal acpid blueman cups cups-pdf cups-filters system-config-printer gnome-themes-extra kvantum-qt5 clang mise mariadb-libs postgresql-libs lazydocker-bin wget curl inetutils eza fzf ripgrep bat fastfetch man less whois plocate bash-completion gnome-calculator localsend-bin bun-bin discord ufw powershell-bin starship docker sqlc docker-compose mongodb-bin mongodb-tools-bin mongodb-compass-bin flatpak optimus-manager nvtop libva-utils ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols networkmanager network-manager-applet nm-connection-editor wireless_tools wpa_supplicant fish
+
+paru -S xorg-xev polybar picom sxhkd
 
 paru -S fontpreview fish-lsp spotify betterlockscreen greenclip ufw-docker mongosh-bin optimus-manager-qt auto-cpufreq
 
@@ -50,15 +52,15 @@ sudo systemctl enable --now bluetooth.service
 
 sudo systemctl enable --now cups.service
 
-# paru -S hyprpicker hypridle rofi-wayland swappy swww wev wayland-protocols wl-clipboard wlr-randr cliphist libsecret hyprlock wlogout grip slurp os-prober hyprls-git
+paru -S hyprpicker hypridle rofi-wayland swappy swww wev wayland-protocols wl-clipboard wlr-randr cliphist libsecret hyprlock wlogout grim slurp nwg-displays waybar hyprpolkitagent sddm gnome-keyring
 
-sudo usermod -aG audio,disk,input,kvm,power,render,video,wheel keshav
+sudo usermod -aG audio,disk,input,kvm,power,render,video,wheel $USER
 
 curl -fsSL https://fnm.vercel.app/install | bash
 
 ## Docker
 sudo systemctl enable docker
-sudo usermod -aG docker ${USER}
+sudo usermod -aG docker $USER
 # Prevent Docker from preventing boot for network-online.target
 sudo mkdir -p /etc/systemd/system/docker.service.d
 sudo tee /etc/systemd/system/docker.service.d/no-block-boot.conf <<'EOF'
